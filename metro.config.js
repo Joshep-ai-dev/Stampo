2a6024767e4b9a2900a6db0acf1a435a663779bf
Bryan Scott Green <deglas9372@outlook.com>
2026-08-28 02:48:40 +0800

    update stampo

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,11 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.blockList = [
+  /[/\\]node_modules[/\\]\.bin(?:[/\\].*)?$/,
+  /\/__tests__\/.*/,
+];
+
 if (!config.resolver.assetExts.includes("csv")) {
   config.resolver.assetExts.push("csv");
 }
